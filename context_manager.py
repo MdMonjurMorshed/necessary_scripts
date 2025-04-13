987265c42a1789459d77d815ff66f1e956daf15b
MD MONJUR MORSHED <mmt3719@gmail.com>
2025-04-13 23:58:30 +0600

    context manager handeling

---
+++ context_manager.py
@@ -0,0 +1,10 @@
+class MyContext:
+    def __enter__(self):
+        print("Entering context")
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        print("Exiting context")
+
+with MyContext():
+    print("Inside the context")
